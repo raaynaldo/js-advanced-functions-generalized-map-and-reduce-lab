c7c66705bc315c99822637d07ba257f93bf9f8f0
raaynaldo <raaynaldo@hotmail.com>
2020-11-27 00:43:40 -0500

    commit tree b637196ee3acd330a143add2c28dc7f9fb14597f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+function map(arr, func) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(func(arr[i]));
+  }
+  return newArr;
+}
+
+function reduce(arr, func, start) {
+  let value = !!start ? start : arr[0];
+  let i = !!start ? 0 : 1;
+  for (; i < arr.length; i++) {
+    value = func(value, arr[i]);
+  }
+  return value;
+}
